312b0686ab948f9ad6f58f83445fce326368425d
SHAIK SAJEETH <sajju.shaik143@gmail.com>
2020-07-10 15:26:04 +0530

    updated sentences to button.js

--- RDIP/Codes/Functionality.js
+++ RDIP/Codes/Functionality.js
@@ -55,75 +55,308 @@ function Function1() {
     obj.employees[7].d;
 
   var hindi = [
-    "राम और श्याम बाजार गयें <br>",
-    "  राम और श्याम गयें बाजार<br>",
-    "बाजार गयें राम और श्याम<br>",
-    " गयें बाजार राम और श्याम<br><br>",
-
-    " राम सोया और श्याम भी<br>",
-    "श्याम सोया और राम भी<br>",
-    "  सोया श्याम और राम भी<br>",
-    " सोया राम और श्याम भी<br><br>",
-
-    " मैंने उसे बताया कि राम सो रहा है<br>",
-    "मैंने उसे बताया कि सो रहा है राम<br>",
-    "उसे मैंने बताया कि राम सो रहा है<br>",
-    "उसे मैंने बताया कि सो रहा है राम<br>",
-    "मैंने बताया उसे कि राम सो रहा है<br>",
-    "मैंने बताया उसे कि सो रहा है राम<br>",
-    "उसे बताया मैंने कि राम सो रहा है<br>",
-    "उसे बताया मैंने कि सो रहा है राम<br>",
-    "बताया मैंने उसे कि राम सो रहा है<br>",
-    "बताया मैंने उसे कि सो रहा है राम<br>",
-    "बताया उसे मैंने कि राम सो रहा है<br>",
-    "बताया उसे मैंने कि सो रहा है राम<br><br>",
-
-    "राम खाकर सोया<br>",
-    "खाकर राम सोया<br>",
-    "राम सोया खाकर<br>",
-    "खाकर सोया राम<br>",
-    "सोया राम खाकर<br>",
-    "सोया खाकर राम<br><br>",
-
-    "बिल्लियों को मारकर कुत्ता सो गया<br>",
-    "मारकर बिल्लियों को कुत्ता सो गया	<br>",
-    "बिल्लियों को मारकर सो गया कुत्ता	<br>",
-    "मारकर बिल्लियों को सो गया कुत्ता<br>",
-    "कुत्ता सो गया बिल्लियों को मारकर	<br>",
-    "कुत्ता सो गया मारकर बिल्लियों को	<br>",
-    "सो गया कुत्ता बिल्लियों को मारकर<br>",
-    "सो गया कुत्ता मारकर बिल्लियों को<br><br>",
-
-    "एक लाल किताब वहाँ है<br>",
-    "एक लाल किताब है वहाँ<br>",
-    "वहाँ है एक लाल किताब<br>",
-    "है वहाँ एक लाल किताब<br><br>",
-
-    "एक बड़ी सी किताब वहाँ है	<br>",
-    "एक बड़ी सी किताब है वहाँ<br>",
-    "बड़ी सी एक किताब वहाँ है<br>",
-    "बड़ी सी एक किताब है वहाँ<br>",
-    "वहाँ है एक बड़ी सी किताब<br>",
-    "वहाँ है बड़ी सी एक किताब<br>",
-    "है वहाँ एक बड़ी सी किताब<br>",
-    "है वहाँ बड़ी सी एक किताब<br>",
+    [
+      "राम और श्याम बाजार गयें <br>",
+      "  राम और श्याम गयें बाजार<br>",
+      "बाजार गयें राम और श्याम<br>",
+      " गयें बाजार राम और श्याम<br><br>",
+    ],
+    [
+      " राम सोया और श्याम भी<br>",
+      "श्याम सोया और राम भी<br>",
+      "  सोया श्याम और राम भी<br>",
+      " सोया राम और श्याम भी<br><br>",
+    ],
+    [
+      " मैंने उसे बताया कि राम सो रहा है<br>",
+      "मैंने उसे बताया कि सो रहा है राम<br>",
+      "उसे मैंने बताया कि राम सो रहा है<br>",
+      "उसे मैंने बताया कि सो रहा है राम<br>",
+      "मैंने बताया उसे कि राम सो रहा है<br>",
+      "मैंने बताया उसे कि सो रहा है राम<br>",
+      "उसे बताया मैंने कि राम सो रहा है<br>",
+      "उसे बताया मैंने कि सो रहा है राम<br>",
+      "बताया मैंने उसे कि राम सो रहा है<br>",
+      "बताया मैंने उसे कि सो रहा है राम<br>",
+      "बताया उसे मैंने कि राम सो रहा है<br>",
+      "बताया उसे मैंने कि सो रहा है राम<br><br>",
+    ],
+    [
+      "राम खाकर सोया<br>",
+      "खाकर राम सोया<br>",
+      "राम सोया खाकर<br>",
+      "खाकर सोया राम<br>",
+      "सोया राम खाकर<br>",
+      "सोया खाकर राम<br><br>",
+    ],
+    [
+      "बिल्लियों को मारकर कुत्ता सो गया<br>",
+      "मारकर बिल्लियों को कुत्ता सो गया	<br>",
+      "बिल्लियों को मारकर सो गया कुत्ता	<br>",
+      "मारकर बिल्लियों को सो गया कुत्ता<br>",
+      "कुत्ता सो गया बिल्लियों को मारकर	<br>",
+      "कुत्ता सो गया मारकर बिल्लियों को	<br>",
+      "सो गया कुत्ता बिल्लियों को मारकर<br>",
+      "सो गया कुत्ता मारकर बिल्लियों को<br><br>",
+    ],
+    [
+      "एक लाल किताब वहाँ है<br>",
+      "एक लाल किताब है वहाँ<br>",
+      "वहाँ है एक लाल किताब<br>",
+      "है वहाँ एक लाल किताब<br><br>",
+    ],
+    [
+      "एक बड़ी सी किताब वहाँ है	<br>",
+      "एक बड़ी सी किताब है वहाँ<br>",
+      "बड़ी सी एक किताब वहाँ है<br>",
+      "बड़ी सी एक किताब है वहाँ<br>",
+      "वहाँ है एक बड़ी सी किताब<br>",
+      "वहाँ है बड़ी सी एक किताब<br>",
+      "है वहाँ एक बड़ी सी किताब<br>",
+      "है वहाँ बड़ी सी एक किताब<br>",
+    ],
   ];
   var y = hindi.join("");
   document.getElementById("output7").innerHTML = y;
 }
 function dropdown() {
+  document.getElementById("b1").innerHTML =
+    "Form a sentence (Declarative or Interrogative or any other type) from the given words";
+
+  document.getElementById("output").innerHTML =
+    "(select the buttons in proper order)";
+
   var x = document.getElementById("mySelect").value;
   if (x == "English") {
-    document.getElementById("info").innerHTML =
-      "Form a sentence (Declarative or Interrogative or any other type) from the given words<br />(select the buttons in proper order)";
+    document.getElementById("b2").innerHTML = "";
+    var english = [
+      [
+        "John ate an apple before afternoon<br>",
+        "before afternoon John ate an apple<br>",
+        "John before afternoon ate an apple<br>",
+      ],
+      [
+        "some students like to study in the night<br>",
+        "at night some students like to study<br>",
+      ],
+      ["John and Mary went to church<br>", "Mary and John went to church<br>"],
+      [
+        "John went to church after eating<br>",
+        "after eating John went to church<br>",
+        "John after eating went to church<br>",
+      ],
+      ["did he go to market<br>", "he did go to market<br>"],
 
-    document.getElementById("engbutttons").style.display = "block";
-    document.getElementById("hindibutttons").style.display = "none";
+      [
+        "the woman who called my sister sells cosmetics<br>",
+        "the woman who sells cosmetics called my sister<br>",
+        "my sister who sells cosmetics called the woman<br>",
+        "my sister who called the woman sells cosmetics<br>",
+      ],
+
+      ["John ate an apple so did she<br>", "she ate an apple so did John<br>"],
+
+      [
+        "the teacher returned the book after she noticed the error<br>",
+        "the teacher noticed the error after she returned the book<br>",
+        "after the teacher returned the book she noticed the error<br>",
+        "after the teacher noticed the error she returned the book<br>",
+        "she returned the book after the teacher noticed the error<br>",
+        "she noticed the error after the teacher returned the book<br>",
+        "after she returned the book the teacher noticed the error<br>",
+        "after she noticed the error the teacher returned the book<br>",
+      ],
+
+      [
+        "I told her that I bought a book yesterday<br>",
+        "I told her yesterday that I bought a book<br>",
+        "yesterday I told her that I bought a book<br>",
+        "I bought a book that I told her yesterday<br>",
+        "I bought a book yesterday that I told her<br>",
+        "yesterday I bought a book that I told her<br>",
+      ],
+    ];
+
+    var max = english.length;
+    var max1 = english.length;
+
+    var i;
+    var min = 0;
+    var m = Math.floor(Math.random() * (+max - +min)) + +min;
+    var a = english;
+    var ran = a[m];
+    var c = a[m].join("");
+    //alert(a[m].length);
+    var len = a[m].length;
+    var max1 = a[m].length;
+    var min1 = 0;
+    var m1 = Math.floor(Math.random() * (+max1 - +min1)) + +min1;
+    //alert(a[m][m1]);
+    var a = a[m][m1];
+    var d = a.replace("<br>", "");
+    //alert("d  : " + d);
+    var arra1 = d.split(" ");
+    var mm = arra1;
+    //alert(arra1);
+    var len1 = arra1.length;
+
+    var ctr = arra1.length,
+      temp,
+      index;
+
+    // While there are elements in the array
+    while (ctr > 0) {
+      // Pick a random index
+      index = Math.floor(Math.random() * ctr);
+      // Decrease ctr by 1
+      ctr--;
+      // And swap the last element with it
+      temp = arra1[ctr];
+      arra1[ctr] = arra1[index];
+      arra1[index] = temp;
+      //  alert(arra1[index]);
+      //add(arra1[index]);
+    }
+    // alert("len : " + len);
+    for (var z = 0; z < len1; z++) {
+      add(arra1[z]);
+    }
+
+    function add(val) {
+      var type = "button";
+      //Create an input type dynamically.
+      var element = document.createElement("input");
+      //Assign different attributes to the element.
+      element.type = type;
+      element.value = val;
+      element.name = type;
+
+      var foo = document.getElementById("b2");
+      document.getElementById("b2").innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;";
+      foo.appendChild(element);
+    }
   } else {
-    document.getElementById("info1").innerHTML =
-      "Form a sentence (Declarative or Interrogative or any other type) from the given words<br />(select the buttons in proper order)";
+    document.getElementById("b2").innerHTML = "";
 
-    document.getElementById("hindibutttons").style.display = "block";
-    document.getElementById("engbutttons").style.display = "none";
+    var hindi = [
+      [
+        "राम और श्याम बाजार गयें<br>",
+        "राम और श्याम गयें बाजार<br>",
+        "बाजार गयें राम और श्याम<br>",
+        "गयें बाजार राम और श्याम<br>",
+      ],
+      [
+        "राम सोया और श्याम भी<br>",
+        "श्याम सोया और राम भी<br>",
+        "सोया श्याम और राम भी<br>",
+        "सोया राम और श्याम भी<br>",
+      ],
+      [
+        "मैंने उसे बताया कि राम सो रहा है<br>",
+        "मैंने उसे बताया कि सो रहा है राम<br>",
+        "उसे मैंने बताया कि राम सो रहा है<br>",
+        "उसे मैंने बताया कि सो रहा है राम<br>",
+        "मैंने बताया उसे कि राम सो रहा है<br>",
+        "मैंने बताया उसे कि सो रहा है राम<br>",
+        "उसे बताया मैंने कि राम सो रहा है<br>",
+        "उसे बताया मैंने कि सो रहा है राम<br>",
+        "बताया मैंने उसे कि राम सो रहा है<br>",
+        "बताया मैंने उसे कि सो रहा है राम<br>",
+        "बताया उसे मैंने कि राम सो रहा है<br>",
+        "बताया उसे मैंने कि सो रहा है राम<br>",
+      ],
+      [
+        "राम खाकर सोया<br>",
+        "खाकर राम सोया<br>",
+        "राम सोया खाकर<br>",
+        "खाकर सोया राम<br>",
+        "सोया राम खाकर<br>",
+        "सोया खाकर राम<br>",
+      ],
+      [
+        "बिल्लियों को मारकर कुत्ता सो गया<br>",
+        "मारकर बिल्लियों को कुत्ता सो गया	<br>",
+        "बिल्लियों को मारकर सो गया कुत्ता<br>",
+        "मारकर बिल्लियों को सो गया कुत्ता<br>",
+        "कुत्ता सो गया बिल्लियों को मारकर<br>",
+        "कुत्ता सो गया मारकर बिल्लियों को<br>",
+        "सो गया कुत्ता बिल्लियों को मारकर<br>",
+        "सो गया कुत्ता मारकर बिल्लियों को<br>",
+      ],
+      [
+        "एक लाल किताब वहाँ है<br>",
+        "एक लाल किताब है वहाँ<br>",
+        "वहाँ है एक लाल किताब<br>",
+        "है वहाँ एक लाल किताब<br>",
+      ],
+      [
+        "एक बड़ी सी किताब वहाँ है<br>",
+        "एक बड़ी सी किताब है वहाँ<br>",
+        "बड़ी सी एक किताब वहाँ है<br>",
+        "बड़ी सी एक किताब है वहाँ<br>",
+        "वहाँ है एक बड़ी सी किताब<br>",
+        "वहाँ है बड़ी सी एक किताब<br>",
+        "है वहाँ एक बड़ी सी किताब<br>",
+        "है वहाँ बड़ी सी एक किताब<br>",
+      ],
+    ];
+
+    var max = hindi.length;
+    var max1 = hindi.length;
+    var i;
+    var min = 0;
+    var m = Math.floor(Math.random() * (+max - +min)) + +min;
+    var a = hindi;
+    var ran = a[m];
+    var c = a[m].join("");
+    //alert(a[m].length);
+    var len = a[m].length;
+    var max1 = a[m].length;
+    var min1 = 0;
+    var m1 = Math.floor(Math.random() * (+max1 - +min1)) + +min1;
+    // alert(a[m][m1]);
+    var a = a[m][m1];
+    var d = a.replace("<br>", "");
+    // alert("d  : " + d);
+    var arra1 = d.split(" ");
+    var mm = arra1;
+    //alert(arra1);
+    var len1 = arra1.length;
+
+    var ctr = arra1.length,
+      temp,
+      index;
+
+    // While there are elements in the array
+    while (ctr > 0) {
+      // Pick a random index
+      index = Math.floor(Math.random() * ctr);
+      // Decrease ctr by 1
+      ctr--;
+      // And swap the last element with it
+      temp = arra1[ctr];
+      arra1[ctr] = arra1[index];
+      arra1[index] = temp;
+      //  alert(arra1[index]);
+      //add(arra1[index]);
+    }
+    // alert("len : " + len);
+    for (var z = 0; z < len1; z++) {
+      add(arra1[z]);
+    }
+
+    function add(val) {
+      var type = "button";
+      //Create an input type dynamically.
+      var element = document.createElement("input");
+      //Assign different attributes to the element.
+      element.type = type;
+      element.value = val;
+      element.name = type;
+
+      var foo = document.getElementById("b2");
+      document.getElementById("b2").innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;";
+      foo.appendChild(element);
+    }
   }
 }
