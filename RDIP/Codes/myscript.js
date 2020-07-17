066438f4ca0c6fac924fe820ad4d58f88187281d
SHAIK SAJEETH <sajju.shaik143@gmail.com>
2020-07-17 13:59:30 +0530

    final commit

---
+++ RDIP/Codes/myscript.js
@@ -0,0 +1,249 @@
+/* Corpus of Sentences:
+English :10
+Hindi : */
+
+var sentence = '{"English":['+
+    '{"a":"John ate an apple before afternoon", "b":"before afternoon John ate an apple", "c":"John before afternoon ate an apple"},' +
+    '{"a":"some students like to study in the night", "b":"in the night some students like to study"},' +
+    '{"a":"John and Mary went to church", "b":"Mary and John went to church"},'+
+    '{"a":"John went to church after eating", "b":"after eating John went to church","c":"John after eating went to church"},' +
+    '{"a":"did he go to market", "b":"he did go to market"},' +
+    '{"a":"the woman who called my sister sells cosmetics", "b":"the woman who sells cosmetics called my sister",'+
+        '"c":"my sister who sells cosmetics called the woman", "d":"my sister who called the woman sells cosmetics"},'+
+    '{"a":"John goes to the library and studies", "b":"John studies and goes to the library"},'+
+    '{"a":"John ate an apple so did she", "b":"she ate an apple so did John"},'+
+    '{"a":"the teacher returned the book after she noticed the error", "b":"the teacher noticed the error after she returned the book",'+
+        '"c":"after the teacher returned the book she noticed the error", "d":"after the teacher noticed the error she returned the book",'+
+        '"e":"she returned the book after the teacher noticed the error", "f":"she noticed the error after the teacher returned the book",'+
+        '"g":"after she returned the book the teacher noticed the error", "h":"after she noticed the error the teacher returned the book"},'+
+    '{"a":"I told her that I bought a book yesterday", "b":"I told her yesterday that I bought a book", "c":"yesterday I told her that I bought a book",'+
+        '"d":"I bought a book that I told her yesterday", "e":"I bought a book yesterday that I told her", "f":"yesterday I bought a book that I told her"}],'+
+    
+    '"Hindi":[' +
+    '{"a":"राम और श्याम बाजार गयें", "b":"राम और श्याम गयें बाजार", "c":"बाजार गयें राम और श्याम", "d":"गयें बाजार राम और श्याम"},'+
+    '{"a":"राम सोया और श्याम भी", "b":"श्याम सोया और राम भी", "c":"सोया श्याम और राम भी", "d":"सोया राम और श्याम भी"},'+
+    '{"a":"मैंने उसे बताया कि राम सो रहा है", "b":"मैंने उसे बताया कि सो रहा है राम", "c":"उसे मैंने बताया कि राम सो रहा है", "d":"उसे मैंने बताया कि सो रहा है राम",'+
+        '"e":"मैंने बताया उसे कि राम सो रहा है", "f":"मैंने बताया उसे कि सो रहा है राम", "g":"उसे बताया मैंने कि राम सो रहा है", "h":"उसे बताया मैंने कि सो रहा है राम",'+
+        '"i":"बताया मैंने उसे कि राम सो रहा है", "j":"बताया मैंने उसे कि सो रहा है राम", "k":"बताया उसे मैंने कि राम सो रहा है", "l":"बताया उसे मैंने कि सो रहा है राम"},'+
+    '{"a":"राम खाकर सोया", "b":"खाकर राम सोया", "c":"राम सोया खाकर", "d":"खाकर सोया राम", "e":"सोया राम खाकर", "f":"सोया खाकर राम"},'+
+    '{"a":"बिल्लियों को मारकर कुत्ता सो गया", "b":"मारकर बिल्लियों को कुत्ता सो गया", "c":"बिल्लियों को मारकर सो गया कुत्ता", "d":"मारकर बिल्लियों को सो गया कुत्ता",'+
+        '"e":"कुत्ता सो गया बिल्लियों को मारकर", "f":"कुत्ता सो गया मारकर बिल्लियों को", "g":"सो गया कुत्ता बिल्लियों को मारकर", "h":"सो गया कुत्ता मारकर बिल्लियों को"},'+
+    '{"a":"एक लाल किताब वहाँ है", "b":"एक लाल किताब है वहाँ", "c":"वहाँ है एक लाल किताब", "d":"है वहाँ एक लाल किताब"},'+
+    '{"a":"एक बड़ी सी किताब वहाँ है", "b":"एक बड़ी सी किताब है वहाँ", "c":"बड़ी सी एक किताब वहाँ है", "d":"बड़ी सी एक किताब है वहाँ", "e":"वहाँ है एक बड़ी सी किताब",'+
+        '"f":"वहाँ है बड़ी सी एक किताब", "g":"है वहाँ एक बड़ी सी किताब", "h":"है वहाँ बड़ी सी एक किताब"}]}';   
+
+/*
+obj = JSON.parse(text);
+document.getElementById("demo").innerHTML =
+obj.English[0].a + " " + obj.Hindi[0].a;
+*/
+
+
+/* Global Variables */
+var language="";
+var current_sentence = "";
+var formed_sentence = "";
+var word_array ;
+var word_count = 0;
+var question = 0;
+var sentences = JSON.parse(sentence);
+
+
+/* Displaying the first message */
+function exp_top(){
+    clear();
+    language = document.getElementById('language').options[document.getElementById('language').selectedIndex].text;
+    if(language == "---Select Language---"){
+        alert('Select a Language');
+        set_display("none");
+        return false;
+    }
+    else if(language == "English" || language == "Hindi"){
+        set_display("initial");
+        document.getElementById("experiment-top").innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words";
+        document.getElementById("experiment-line").innerHTML = "(select the buttons in proper order)";
+        sentence_selection(language);
+    }
+}
+
+/* Selection of a random sentence */
+function sentence_selection(language){
+    document.getElementById('experiment-sentence').innerHTML = "";
+    if(language == "English"){
+        question = Math.floor(Math.random() * 10);                        // Randomly selecting an english sentence.
+        current_sentence = sentences.English[question].a;
+        sentence_to_buttons( sentences.English[question].a );
+    }
+    else if(language == "Hindi"){
+        question = Math.floor(Math.random() * 7);                         // Randomly selecting an hindi sentence.
+        current_sentence = sentences.Hindi[question].a;
+        sentence_to_buttons( sentences.Hindi[question].a );
+    }
+}
+
+/* Shuffling the words in the sentence */
+function sentence_to_buttons( str ){
+    var arr = str.split(" ");
+    word_array = shuffle(arr);
+    adding_buttons(word_array);
+}
+
+/*Converting the shuffled words into sentences */
+function adding_buttons(arr){
+    var j = "1";
+    for(i=0;i<arr.length;i++){
+        var button = document.createElement("button");
+        button.setAttribute("id",j);
+        button.setAttribute("value",arr[i]);
+        j += String(parseFloat(j) + 1);
+        button.innerHTML = arr[i];
+        document.getElementById('experiment-sentence').appendChild(button);
+    }
+    document.getElementById('formed-sentence').value = String();
+    word_count = 0;
+}
+
+/* Shuffling words */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+/* Displaying second message */
+function second_message(id1){
+    if(id1 == "experiment-sentence")
+        return ;
+    document.getElementById('second-msg').innerHTML = "Formed Sentence ";
+    document.getElementById('second-line').innerHTML = "(after selecting words):";
+    document.getElementById("reform-button").style.display = "initial";
+}
+
+/* Displaying the formed sentence */
+function a(id1){
+    if(id1 == "experiment-sentence")
+        return ;
+    if(word_count == 0){
+        document.getElementById('formed-sentence').innerHTML = String( document.getElementById(id1).value );
+    }
+    else{
+        document.getElementById('formed-sentence').innerHTML = String( document.getElementById('formed-sentence').innerHTML ) + " " + String( document.getElementById(id1).value );
+    }
+    formed_sentence = document.getElementById('formed-sentence').innerHTML;
+    document.getElementById(id1).style.display = "none";
+
+    word_count += 1;
+
+    if(word_count == word_array.length){
+        document.getElementById("check-correctness").style.display = "initial";
+    }
+}
+
+/* Re-form button functionality */
+function reform(){
+    clear();
+    document.getElementById('experiment-sentence').innerHTML = "";
+    adding_buttons(word_array);                                             // Jumbling back words to the previous order.
+}
+
+/* Validating the formed sentence */ 
+function check_sentence(){
+    var status = false;
+    if(language == "English"){
+        for(sent in sentences.English[question]){
+            if(sentences.English[question][sent] == formed_sentence){
+                status = true;
+                break;
+            }
+        }
+
+        if(status == true){
+            document.getElementById('right-answer').style.display = "initial";
+        }
+        else{
+            document.getElementById('wrong-answer').style.display = "initial";
+            document.getElementById('correct-sentence').style.display = "initial";         // Invoked when 'check the correctness of the sentence is clicked.
+        }
+        return;
+    }
+    else if(language == "Hindi"){
+        for(sent in sentences.Hindi[question]){
+            if(sentences.Hindi[question][sent] == formed_sentence){
+                status = true;
+                break;
+            }
+        }
+
+        if(status == true){
+            document.getElementById('right-answer').style.display = "initial";
+        }
+        else{
+            document.getElementById('wrong-answer').style.display = "initial";
+            document.getElementById('correct-sentence').style.display = "initial";          // Invoked when 'check the correctness of the sentence is clicked.
+        }
+        return true;
+    }
+    
+    
+}
+
+/*Displaying correct sentences */
+function get_sentences(){
+    document.getElementById('correct-sentences').style.display = "initial";
+
+    var name = document.getElementById('correct-sentence').innerHTML;
+
+    if(name == "Get the correct sentence" || name == "Get answers"){                            //Changing the name of the button accordingly.
+        document.getElementById('correct-sentence').innerHTML = "Hide the correct sentence";
+
+        if(language == "English"){
+            for(sent in sentences.English[question]){
+                var t = document.createTextNode(sentences.English[question][sent]);
+                document.getElementById('correct-sentences').appendChild(t);
+                var line = document.createElement("br");
+                document.getElementById('correct-sentences').appendChild(line);
+            }
+            return;
+        }
+        else if(language == "Hindi"){
+            for(sent in sentences.Hindi[question]){
+                var t = document.createTextNode(sentences.Hindi[question][sent]);
+                document.getElementById('correct-sentences').appendChild(t);
+                var line = document.createElement("br");
+                document.getElementById('correct-sentences').appendChild(line);
+            }
+            return;
+        }
+    }
+    else if(name == "Hide the correct sentence"){
+        document.getElementById('correct-sentence').innerHTML = "Get answers";
+        document.getElementById('correct-sentences').innerHTML = "";
+    }
+}
+
+function clear(){
+    document.getElementById("second-msg").innerHTML = "";
+    document.getElementById("second-line").innerHTML = "";
+    document.getElementById('formed-sentence').innerHTML = "";
+    document.getElementById("reform-button").style.display = "none";
+    document.getElementById("check-correctness").style.display = "none";
+    document.getElementById('right-answer').style.display = "none";
+    document.getElementById('wrong-answer').style.display = "none";
+    document.getElementById('correct-sentence').style.display = "none";
+    document.getElementById('correct-sentences').style.display = "none";
+    document.getElementById('correct-sentence').innerHTML = "Get the correct sentence";
+    document.getElementById('correct-sentences').innerHTML = "";
+}
+
+function set_display(val){
+    document.getElementById("experiment-top").style.display = val;
+    document.getElementById("experiment-line").style.display = val;
+    document.getElementById("experiment-sentence").style.display = val;
+    document.getElementById("second-msg").style.display = val;
+    document.getElementById("second-line").style.display = val;
+}
---
+++ RDIP_1/Codes/tokens.js
@@ -0,0 +1,234 @@
+/* 3 Corpus */
+
+var paragraphs = ["A mouse was having a very bad time. She could find no food at all. She looked here and there, but there was no food, and she grew very thin." +
+                        " At last the mouse found a basket, full of corn. There was a small hole in the basket, and she crept in. She could just get through the "+
+                        "hole. Then she began to eat the corn. Being very hungry, she ate a great deal, and went on eating and eating. She had grown very fat "+
+                        "before she felt that she had had enough. When the mouse tried to climb out of the basket, she could not. She was too fat to pass through"+
+                        " the hole. \"How shall I climb out?\" said the mouse. \"oh, how shall I climb out?\" Just then a rat came along, and he heard the mouse."+
+                        " \"Mouse,\" said the rat, \"if you want to climb out of the basket, you must wait till you have grown as thin as you were when you went in.\"",
+                "A wolf carried off a lamb. The lamb said, \" I know you are going to eat me, but before you eat me I would like to hear you play the flute. I"+
+                        " have heard that you can play the flute better than anyone else, even the shepherd himself.\" The wolf was so pleased at this that he took"+
+                        " out his flute and began to play. When he had done, the lamb insisted him to play once more and the wolf played again. The shepherd and "+
+                        "the dogs heard the sound, and they came running up and fell on the wolf and the lamb was able to get back to the flock.",
+                "A man had a little dog, and he was very fond of it. He would pat its head, and take it on his knee, and talk to it. Then he would give it little "+
+                        "bits of food from his own plate. A donkey looked in at the window and saw the man and the dog. \"Why does he not make a pet of me?\" said"+
+                        " the donkey. \"It is not fair. I work hard, and the dog only wags its tail, and barks, and jumps on its master's knee. It is not fair.\" "+
+                        "Then the donkey said to himself, \"If I do what the dog does, he may make a pet of me.\" So the donkey ran into the room. It brayed as"+
+                        " loudly as it could. It wagged its tail so hard that it knocked over a jar on the table. Then it tried to jump on to its master's knee."+
+                        " The master thought the donkey was mad, and he shouted, \"Help! Help!\" Men came running in with sticks, and they beat the donkey till it"+
+                        " ran out of the house, and they drove it back to the field. \"I only did what the dog does,\" said the donkey,\" and yet they make a pet "+
+                        "of the dog, and they beat me with sticks. It is not fair."];
+                    
+/* Accesing : alert(paragraphs[2]); */
+
+
+var stemmer = new Snowball('English');
+// stemmer.setCurrent('abbreviations');
+// stemmer.stem();
+// alert(stemmer.getCurrent());
+
+var corpus = "";
+var no_of_tokens = 0;
+var no_of_types = 0;
+var no_of_stemmed_types = 0;
+
+function display_corpus(){
+
+        corpus = document.getElementById('corpus').options[document.getElementById('corpus').selectedIndex].text;
+
+        if(corpus == "---Select a corpus---"){
+                alert("Select a corpus");
+                clear();
+                return false;
+        }
+
+        else if(corpus == "Corpus 1"){
+                document.getElementById("experiment-corpus").innerHTML = paragraphs[0];
+        }
+
+        else if(corpus == "Corpus 2"){
+                document.getElementById("experiment-corpus").innerHTML = paragraphs[1];
+        }
+
+        else if(corpus == "Corpus 3"){
+                document.getElementById("experiment-corpus").innerHTML = paragraphs[2];
+        }
+
+        set();
+
+        display_table1();
+        return true;
+
+}
+
+function display_table1(){
+
+        document.getElementById("experiment-table-head").innerHTML = "Enter the number of tokens and types for the above corpus:";
+        document.getElementById("experiment-table").style.display = "initial";
+}
+
+function calculate_tokens_types(){
+
+        var index = parseInt(corpus[corpus.length - 1]);
+        var arr = paragraphs[index - 1].split(/[\s,.?\"]+/);
+
+        for (var i = 0; i < arr.length-1; i++) {
+                arr[i] = arr[i].toLowerCase();
+        }
+
+        no_of_tokens = arr.length - 1;
+
+        arr = new Set(arr);
+        arr = Array.from(arr);
+
+        no_of_types = arr.length - 1;
+
+}
+
+function check_tokens_and_types(){
+
+        var flag = 0 ;
+
+        if(document.getElementById('token').value == no_of_tokens){
+                document.getElementById('token').style.backgroundColor = "#008000";
+                flag += 1;
+        }
+        else{
+                document.getElementById('token').style.backgroundColor = "#FF0000";
+        }
+
+        if(document.getElementById('type').value == no_of_types){
+                document.getElementById('type').style.backgroundColor = "#008000";
+                flag += 1;
+        }
+        else{
+                document.getElementById('type').style.backgroundColor = "#FF0000";
+        }
+
+        if(flag == 2){
+                document.getElementById('right-answer').style.display = "initial";
+                document.getElementById('wrong-answer').style.display = "none";
+                document.getElementById('continue').style.display = "initial";
+        }
+        else{
+                document.getElementById('right-answer').style.display = "none";
+                document.getElementById('wrong-answer').style.display = "initial";
+                document.getElementById('continue').style.display = "none";
+        }
+
+        document.getElementById('token').style.color = "#FFFFFF";
+        document.getElementById('type').style.color = "#FFFFFF";
+
+}
+
+function types_stemming(){
+
+        document.getElementById('stemming-sentence').style.display = "initial";
+        document.getElementById('stemming-sentence').innerHTML = "Now, consider all the tokens with the same 'root' word to be of the same type. Recalculate the number of types.";
+        document.getElementById('right-answer').style.display = "none";
+        document.getElementById('wrong-answer').style.display = "none";
+        document.getElementById('continue').style.display = "none";
+        document.getElementById('submit').style.display = "none";
+        document.getElementById('new-types').innerHTML = "#new types:";
+        document.getElementById('new-types-box').style.display = "initial";
+        document.getElementById('continue-submit').style.display = "initial";
+
+        create_stemmed_corpus();
+
+}
+
+function create_stemmed_corpus(){
+
+        var stemmed_corpus = [];
+        
+        var unneccesary_words = new Set(["of","the","to","up","can","than","did","me","you","off","does","very"])
+
+        var index = parseInt(corpus[corpus.length - 1]);
+        var arr = paragraphs[index - 1].split(/[\s,.?\"]+/);
+
+        for (var i = 0; i < arr.length-1; i++) {
+                arr[i] = arr[i].toLowerCase();
+        }
+        
+        for(var i=0; i<arr.length-1 ; i++){
+
+                if(unneccesary_words.has(arr[i])){
+                        continue;
+                }
+
+                // alert(arr[i]);
+
+                stemmer.setCurrent(arr[i]);
+                stemmer.stem();
+                stemmed_corpus.push( stemmer.getCurrent() );
+
+                // alert(stemmed_corpus[i]);
+        }
+
+        stemmed_corpus = new Set(stemmed_corpus);
+        stemmed_corpus = Array.from(stemmed_corpus);
+        
+        no_of_stemmed_types = stemmed_corpus.length;        
+
+}
+
+function check_stemmed_types(){
+
+        if(document.getElementById('new-types-box').value == no_of_stemmed_types){
+                document.getElementById('new-types-box').style.backgroundColor = "#008000";
+                document.getElementById('right-answer1').style.display = "initial";
+                document.getElementById('wrong-answer1').style.display = "none";
+        }
+        else{
+                document.getElementById('new-types-box').style.backgroundColor = "#FF0000";
+                document.getElementById('right-answer1').style.display = "none";
+                document.getElementById('wrong-answer1').style.display = "initial";
+        }
+
+        document.getElementById('new-types-box').style.color = "#FFFFFF";
+
+}
+
+function set(){
+
+        document.getElementById('token').value = "";
+        document.getElementById('type').value = "";
+        document.getElementById('new-types-box').value = "";
+        document.getElementById('type').style.backgroundColor = "#FFFFFF";
+        document.getElementById('token').style.backgroundColor = "#FFFFFF";
+        document.getElementById('new-types-box').style.backgroundColor = "#FFFFFF";
+        document.getElementById('token').style.color = "#444444";
+        document.getElementById('type').style.color = "#444444";
+        document.getElementById('new-types-box').style.color = "#444444";
+        document.getElementById('submit').style.display = "flex";
+        document.getElementById('right-answer').style.display = "none";
+        document.getElementById('wrong-answer').style.display = "none";
+        document.getElementById('right-answer1').style.display = "none";
+        document.getElementById('wrong-answer1').style.display = "none";
+        document.getElementById('continue').style.display = "none";
+        document.getElementById('stemming-sentence').style.display = "none";
+        document.getElementById('new-types').innerHTML = "";
+        document.getElementById('new-types-box').style.display = "none";
+        document.getElementById('continue-submit').style.display = "none";
+
+}
+
+function clear(){
+
+        document.getElementById("experiment-corpus").innerHTML = "";
+        document.getElementById("experiment-table-head").innerHTML = "";
+        document.getElementById("experiment-table").style.display = "none";
+        document.getElementById('type').style.backgroundColor = "#FFFFFF";
+        document.getElementById('token').style.backgroundColor = "#FFFFFF";
+        document.getElementById('new-types-box').style.backgroundColor = "#FFFFFF";
+        document.getElementById('right-answer').style.display = "none";
+        document.getElementById('wrong-answer').style.display = "none";
+        document.getElementById('right-answer1').style.display = "none";
+        document.getElementById('wrong-answer1').style.display = "none";
+        document.getElementById('continue').style.display = "none";
+        document.getElementById('stemming-sentence').style.display = "none";
+        document.getElementById('new-types').innerHTML = "";
+        document.getElementById('new-types-box').style.display = "none";
+        document.getElementById('continue-submit').style.display = "none";
+
+}
