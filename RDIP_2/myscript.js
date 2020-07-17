914b5296a992719b1d4639b0b9b0818233897a99
SHAIK SAJEETH <sajju.shaik143@gmail.com>
2020-07-17 14:10:51 +0530

    corpus added

---
+++ RDIP_2/myscript.js
@@ -0,0 +1,7 @@
+var sentence = '{"English":['+
+                    '{"a":"The child liked the chocolate.", "b":"She was stopped by the bravest knight.", "c":"Mary baked a cake for his birthday",' + 
+                        '"d":"She decorated the cake carefully", "5":"Mary wore a dress with polka dots."}],' +
+
+                '"Hindi":[' +
+                    '{"a":"राम ने सीता के लिए फल तोड़ा।", "b":"छोटे बच्चे पाठशाला जल्दी आयेंगे।", "c":"मेहनत का फल मीठा होता है।", "d":"वाह! वह खूबसूरत है।",' +
+                        '"e":"पेड़ से पत्ते गिर गए।"}]}';
